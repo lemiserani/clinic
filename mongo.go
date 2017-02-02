1fa116cb97db11e5a70087b1313e54a4614e7f5a
Leandro Miserani <leandro.miserani@walmart.com>
2017-02-02 18:20:47 -0200

    Active MongoDB

--- mongo.go
+++ mongo.go
@@ -1,17 +1,20 @@
 package main
 
-import mongo "gopkg.in/mgo.v2"
+import (
+	"log"
+	"os"
+
+	mongo "gopkg.in/mgo.v2"
+)
 
 //GetSession creates a new mongo session and panics if connection error occurs
 func GetSession() *mongo.Session {
-	// Connect to our local mongo
-	s, _ := mongo.Dial("mongodb://localhost")
+	mongodb := os.Getenv("MONGODB_URI")
+	s, _ := mongo.Dial(mongodb)
 
-	// Check if connection error, is mongo running?
-	// if err != nil {
-	// 	panic(err)
-	// }
-
+	if mongodb == "" {
+		log.Fatal("$MONGODB_URI must be set")
+	}
 	// Deliver session
 	return s
 }
